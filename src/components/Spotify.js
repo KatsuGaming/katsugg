3e7f2287974b7eb9209d5a98aabafbe215807cd3
KatsuGaming <60704389+KatsuGaming@users.noreply.github.com>
2021-01-23 17:21:51 -0500

    React Version
    Katsu-Gaming React Version

---
+++ src/components/Spotify.js
@@ -0,0 +1,14 @@
+const Spotify = () => {
+  return (
+    <iframe
+      src="https://open.spotify.com/embed-podcast/show/1tyCn7Y6iTWynpzz4Fl0La"
+      width="100%"
+      height="150px"
+      frameborder="0"
+      allowtransparency="true"
+      allow="encrypted-media"
+      title="podcastStream"
+    ></iframe>
+  );
+};
+export default Spotify;
